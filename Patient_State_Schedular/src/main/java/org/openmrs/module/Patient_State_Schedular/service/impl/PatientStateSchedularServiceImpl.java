4c6a91b95a9be6cf31838d667a522824f954508f
Sushant <sushantpatil1214@gmail.com>
2019-08-29 14:37:43 +0530

    Sushant | added schedular for appoinment

--- Patient_State_Schedular/src/main/java/org/openmrs/module/Patient_State_Schedular/service/impl/PatientStateSchedularServiceImpl.java
+++ Patient_State_Schedular/src/main/java/org/openmrs/module/Patient_State_Schedular/service/impl/PatientStateSchedularServiceImpl.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import org.openmrs.module.Patient_State_Schedular.model.Erpdrug_Order;
-import org.openmrs.module.Patient_State_Schedular.model.PatientStatusState;
 import org.openmrs.module.Patient_State_Schedular.service.PatientStateSchedularService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,11 +23,77 @@ public class PatientStateSchedularServiceImpl implements PatientStateSchedularSe
 		try {
 				
 			
-			id= (int) entityManager.createNativeQuery("INSERT into patient_status_state (patient_id,patient_state,patient_status,creator,date_created,uuid) SELECT patient_id,'ABANDONED','TARV',0,NOW(),UUID() FROM erpdrug_order" + 
-					" INNER JOIN" + 
-					" (" + 
-					" SELECT MAX(id) as id FROM erpdrug_order  WHERE  dispensed_date <= DATE_SUB(CURDATE(),INTERVAL 90 DAY) AND arv_dispensed=true AND erpdrug_order.patient_id NOT IN (select pss1.patient_id from patient_status_state pss1 where patient_state='ABANDONED' and patient_status='TARV' )   GROUP BY patient_id) last_updates" + 
-					" ON last_updates.id = erpdrug_order.id", Erpdrug_Order.class).executeUpdate();
+			id= (int) entityManager.createNativeQuery("INSERT into\n" + 
+					"   patient_status_state (patient_id, patient_state, patient_status, creator, date_created, uuid) \n" + 
+					"   SELECT\n" + 
+					"      patient_id, " + 
+					"      'ABANDONED',\n" + 
+					"      'TARV',\n" + 
+					"      0,\n" + 
+					"      NOW(),\n" + 
+					"      UUID() \n" + 
+					"   FROM\n" + 
+					"      erpdrug_order \n" + 
+					"      INNER JOIN\n" + 
+					"         (\n" + 
+					"            SELECT\n" + 
+					"               MAX(id) as id \n" + 
+					"            FROM\n" + 
+					"               erpdrug_order \n" + 
+					"            WHERE\n" + 
+					"               dispensed_date <= DATE_SUB(CURDATE(), INTERVAL 90 DAY) \n" + 
+					"               AND arv_dispensed = true \n" + 
+					"               AND erpdrug_order.patient_id NOT IN \n" + 
+					"               (\n" + 
+					"                  select\n" + 
+					"                     pss1.patient_id \n" + 
+					"                  from\n" + 
+					"                     patient_status_state pss1 \n" + 
+					"                  where\n" + 
+					"                     patient_state = 'ABANDONED' \n" + 
+					"                     and patient_status = 'TARV' \n" + 
+					"               )\n" + 
+					"            GROUP BY\n" + 
+					"               patient_id\n" + 
+					"         )\n" + 
+					"         last_updates \n" + 
+					"         ON last_updates.id = erpdrug_order.id \n" + 
+					"      UNION\n" + 
+					"      SELECT\n" + 
+					"         patient_id,\n" + 
+					"         'ABANDONED',\n" + 
+					"         'TARV',\n" + 
+					"         0,\n" + 
+					"         NOW(),\n" + 
+					"         UUID() \n" + 
+					"      FROM\n" + 
+					"         patient_appointment \n" + 
+					"         INNER JOIN\n" + 
+					"            (\n" + 
+					"               SELECT\n" + 
+					"                  MAX(patient_appointment_id) as id \n" + 
+					"               FROM\n" + 
+					"                  patient_appointment a \n" + 
+					"                  inner join\n" + 
+					"                     appointment_service b \n" + 
+					"                     on a.appointment_service_id = b.appointment_service_id \n" + 
+					"                      \n" + 
+					"               WHERE\n" + 
+					"                  start_date_time <= DATE_SUB(CURDATE(), INTERVAL 90 DAY ) and b.name = 'Consulta Clínica' AND a.patient_id NOT IN \n" + 
+					"                  (\n" + 
+					"                     select\n" + 
+					"                        pss1.patient_id \n" + 
+					"                     from\n" + 
+					"                        patient_status_state pss1 \n" + 
+					"                     where\n" + 
+					"                        patient_state = 'ABANDONED' \n" + 
+					"                        and patient_status = 'TARV'\n" + 
+					"                  )\n" + 
+					"               GROUP BY\n" + 
+					"                  patient_id\n" + 
+					"            )\n" + 
+					"            last_updates \n" + 
+					"            ON last_updates.id = patient_appointment.patient_appointment_id;", Erpdrug_Order.class).executeUpdate();
 			} catch (Exception e) {
 				logger.error(e.toString());
 			}
