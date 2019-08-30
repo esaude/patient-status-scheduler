20bc00021a47c1f54c69b4d8790d370a2297dc23
Sushant <sushantpatil1214@gmail.com>
2019-08-31 00:04:48 +0530

    Sushant | SH Fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 # Link the app folder
  sudo mkdir /opt/PatientStatusScheduler
  sudo cp ../Patient_State_Schedular/target/Patient_State_Schedular-0.0.1-SNAPSHOT.jar /opt/PatientStatusScheduler
+ sudo chmod -R 777 /opt/PatientStatusScheduler
+ sudo chown -R bahmni:bahmni /opt/PatientStatusScheduler
  sudo cp PatientStateSchedular /etc/init.d
  sudo chmod 777 PatientStateSchedular
 
