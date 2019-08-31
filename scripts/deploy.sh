a277515b6e22254178e28a45c16df493bca97711
Sushant <sushantpatil1214@gmail.com>
2019-08-31 13:26:23 +0530

    Sushant | added schedular for appoinment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Link the app folder
- sudo mkdir /opt/PatientStatusScheduler
- sudo cp ../Patient_State_Schedular/target/Patient_State_Schedular-0.0.1-SNAPSHOT.jar /opt/PatientStatusScheduler
- sudo chmod -R 777 /opt/PatientStatusScheduler
- sudo chown -R bahmni:bahmni /opt/PatientStatusScheduler
+ sudo mkdir /opt/PatientStatusState
+ sudo cp ../Patient_State_Schedular/target/Patient_State_Schedular-0.0.1-SNAPSHOT.jar /opt/PatientStatusState
+ sudo chmod -R 777 /opt/PatientStatusState
+ sudo chown -R bahmni:bahmni /opt/PatientStatusState
  sudo cp PatientStateSchedular /etc/init.d
  sudo chmod 777 /etc/init.d/PatientStateSchedular
 
