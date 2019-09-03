7e2b2deefe481c5a7674fbdeaec44a62ed70b764
Sushant <sushantpatil1214@gmail.com>
2019-09-03 11:12:57 +0530

    Sushant | MPOC-503 change ownership of folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
  sudo mkdir /opt/PatientStatusState
  sudo cp ../Patient_State_Schedular/target/Patient_State_Schedular-0.0.1-SNAPSHOT.jar /opt/PatientStatusState
  sudo chmod -R 777 /opt/PatientStatusState
- sudo chown -R bahmni:bahmni /opt/PatientStatusState
+ sudo chown -R root:root /opt/PatientStatusState
  sudo cp PatientStateSchedular /etc/init.d
  sudo chmod 777 /etc/init.d/PatientStateSchedular
 
